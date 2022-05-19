5c89f73d97f213bca3c9dcb5b1475770a3459157
kubognom <90317142+kubognom@users.noreply.github.com>
2022-05-19 12:02:00 +0300

    pr6

--- PR6/pr66.py
+++ PR6/pr66.py
@@ -1,8 +1,11 @@
 from prettytable import PrettyTable  # Импортируем установленный модуль.
 th = ['раздел ', 'его размер, байт', 'свободен']
-td = ['1',          '16 000',                'yes',
-      '2',          '23 000',               'yes',
-      '3',          '15 000',              'yes']
+td = ['1',          '16000',                'yes',
+      '2',          '23000',               'yes',
+      '3',          '15000',              'yes',
+      '4',          '40000',              'yes',
+      '5',          '30000',              'yes',
+      '6',          '50000',              'yes']
 columns = len(th)  # Подсчитаем кол-во столбцов на будущее.
 table = PrettyTable(th)  # Определяем таблицу.
 td_data = td[:]
@@ -17,7 +20,7 @@ a = 0
 import PySimpleGUI as sg
 import os
 while True:
-    vib = int(input('1 - add | 2 - start | 3 - replace  >>  '))
+    vib = int(input('  1 - add |      2 - start   | 3 - replace  >>  '))
     if vib == 1:
         filename = sg.popup_get_file('Добавьте файлы в очередь')
         a = a + 1
@@ -28,17 +31,33 @@ while True:
 
         index = name.index('.')
         file.append('%6s' % name[:index]+ ' ')
-        print(colls)
-        print(file)
-        print(size)
+
     if vib== 3:
         vib2 = int(input('>> '))
         aa = colls.pop(vib2-1)
         colls.append(aa)
         bb = file.pop(vib2-1)
         file.append(bb)
-        print(colls)
-        print(file)
+
+    if vib ==2:
+        i = 0
+        prov2 = size.pop(0)
+        colls.pop(0)
+        file.pop(0)
+        while i<len(td):
+            prov3 = int(td[i+1])
+            i+=3
+            print(prov2, ' < ', prov3)
+            if prov3 > int(prov2):
+                print('yes')
+            else:
+                print('no')
+    print(colls)
+    print(file)
+    print(size)
+
+
+
 
 
 
